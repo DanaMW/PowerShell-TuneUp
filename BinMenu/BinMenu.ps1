e2190d8fa6048d492e943ef7aa4ab5e933cd7d55
dana <danamw@gmail.com>
2018-08-23 10:49:39 -0700

    More fine updates

--- BinMenu/BinMenu.ps1
+++ BinMenu/BinMenu.ps1
@@ -13,13 +13,13 @@
         Still under development.
 #>
 param([string]$Base)
-$FileVersion = "0.5.3"
+$FileVersion = "0.5.4"
 Function Get-ScriptDir {
     Split-Path -parent $PSCommandPath
 }
-<# #######[Set-ConWin]###[Window Resizer]############################## #>
-$tmpWidth = 104
+<# #[Set-ConWin]#[Window Resizer]# #>
 $tmpHeight = 37
+$tmpWidth = 104
 if ($tmpWidth -eq "") { $tmpWidth = 107 }
 if ($tmpHeight -eq "") { $tmpHeight = 45 }
 $pshost = get-host
@@ -29,11 +29,10 @@ $newsize.height = 2000
 $tmp = ($tmpWidth * 2)
 $newsize.width = $tmp
 $pswindow.buffersize = $newsize
-$newsize = ($pswindow.windowsize)
+$newsize = $pswindow.windowsize
 $newsize.height = $tmpHeight
 $newsize.width = $tmpWidth
 $pswindow.windowsize = $newsize
-<# ################################################################ #>
 Clear-Host
 <# Set The BASE folder here or the script will use current by default #>
 $Base = "C:\bin"
@@ -89,7 +88,6 @@ Write-host "Temp" $temp
 Write-host "B" $b
 Write-host "C" $c
 Write-host "Row" $row
-#break
 <# Draw the menu outline now. #>
 Clear-Host
 Write-Host $NormalLine
@@ -277,9 +275,7 @@ Function MyMaker {
                 $NameFix = $NameFix.replace(".exe", "")
                 Write-Host "Adding to Menu: " $NameFix
                 $Writer.WriteLine("[" + $i + "A]=" + $NameFix)
-                <# $Writer.WriteLine("[" + $i + "A]=" + $_.name.replace(".exe", "")) #>
                 $Writer.WriteLine("[" + $i + "B]=" + $_.fullname)
-                <# export-Csv -Path $FileMen -Append -Delimiter ", " -InputObject $_ #>
                 $i++
                 return
             }
@@ -299,42 +295,39 @@ if ($NoINI) {
     MyMaker
 }
 Function MySysInf {
+    $FileVersion = "0.1.6"
     $ESC = [char]27
     $nline = "$ESC[31m#=====================================================================#$ESC[37m"
     $dline = "$ESC[31m| $ESC[37m| $ESC[31m#=============================================================$ESC[31m# $ESC[37m| $ESC[31m|"
     $fline = "$ESC[31m| $ESC[37m#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# $ESC[31m| $ESC[37m"
     $tline = "$ESC[31m| $ESC[37m#=-=-=-=-=-=-=-=-=-=-=-<$ESC[36m[$ESC[37mSystem Information$ESC[36m]$ESC[31m$ESC[37m>=-=-=-=-=-=-=-=-=-=-=#$ESC[31m |$ESC[37m"
     $sline = "$ESC[31m| $ESC[37m| $ESC[31m|                                                             $ESC[31m| $ESC[37m|$ESC[31m |$ESC[37m"
-    $host.ui.RawUI.WindowTitle = "System Information v." + $FileVersion
-    <# #######[Set-ConWin]###[Window Resizer]############################## #>
-    $tmpWidth = 72
-    $tmpHeight = 48
-    if ($tmpWidth -eq "") { $tmpWidth = 107 }
-    if ($tmpHeight -eq "") { $tmpHeight = 45 }
-    $pshost = get-host
-    $pswindow = $pshost.ui.rawui
-    $newsize = $pswindow.buffersize
-    $newsize.height = 2000
+    $host.ui.RawUI.WindowTitle = "System Information Version " + $FileVersion
+    <# #[Set-ConWin]#[Window Resizer]# #>
+    $tmpHeight = "46"
+    $tmpWidth = "72"
+    if ($tmpWidth -eq "") { $tmpWidth = "107" }
+    if ($tmpHeight -eq "") { $tmpHeight = "45" }
+    $pshost = (get-host)
+    $pswindow = ($pshost.ui.rawui)
+    $newsize = ($pswindow.buffersize)
+    $newsize.height = "2000"
     $tmp = ($tmpWidth * 2)
-    $newsize.width = $tmp
-    $pswindow.buffersize = $newsize
+    $newsize.width = "$tmp"
+    $pswindow.buffersize = ($newsize)
     $newsize = ($pswindow.windowsize)
-    $newsize.height = $tmpHeight
-    $newsize.width = $tmpWidth
-    $pswindow.windowsize = $newsize
-    <# ################################################################ #>
+    $newsize.height = ($tmpHeight)
+    $newsize.width = ($tmpWidth)
+    $pswindow.windowsize = ($newsize)
     $Base = "C:\bin\"
     Get-CimInstance Win32_OperatingSystem | Format-List
     Clear-Host
-    $tmpfile = ("$base" + "Get-Sysinfo.tmp")
     $computerSystem = (Get-CimInstance CIM_ComputerSystem)
     $computerBIOS = (Get-CimInstance CIM_BIOSElement)
     $computerOS = (Get-CimInstance CIM_OperatingSystem)
     $computerCPU = (Get-CimInstance CIM_Processor)
     $computerHDD1 = (Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'C:'")
     $computerHDD2 = (Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'D:'")
-    #$(Get-CimInstance Win32_NetworkAdapter) |  Format-Table -Property name, InterfaceDescription, status, linkspeed
-    #$(Get-NetAdapter) | Select-Object -Property name, InterfaceDescription, Status, LinkSpeed |  Export-Csv $tmpfile
     $Net1 = $(Get-NetAdapter)[0] | Select-Object -Property name, InterfaceDescription, Status, LinkSpeed
     $Net2 = $(Get-NetAdapter)[1] | Select-Object -Property name, InterfaceDescription, Status, LinkSpeed
     $Net3 = $(Get-NetAdapter)[2] | Select-Object -Property name, InterfaceDescription, Status, LinkSpeed
@@ -342,20 +335,28 @@ Function MySysInf {
     $tt = "$ESC[31m[$ESC[37m"
     $Con11 = "$tt" + "NetName$ESC[31m]$ESC[37m: $ESC[33m" + ($Net1.name)
     $Con12 = "$tt" + "Descrip$ESC[31m]$ESC[37m: $ESC[36m" + ($net1.InterfaceDescription)
-    $Con13 = "$tt" + "Status $ESC[31m]$ESC[37m: $ESC[36m" + ($net1.Status)
-    $Con14 = "$tt" + "Speed  $ESC[31m]$ESC[37m: $ESC[36m" + ($net1.LinkSpeed)
+    if ($net1.Status -eq "Not Present") { $N1tmp = "Disabled"}
+    else { $N1tmp = ($net1.Status) }
+    $Con13 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($N1tmp)
+    $Con14 = "$tt" + "Speed..$ESC[31m]$ESC[37m: $ESC[36m" + ($net1.LinkSpeed)
     $Con21 = "$tt" + "NetName$ESC[31m]$ESC[37m: $ESC[33m" + ($Net2.name)
     $Con22 = "$tt" + "Descrip$ESC[31m]$ESC[37m: $ESC[36m" + ($net2.InterfaceDescription)
-    $Con23 = "$tt" + "Status $ESC[31m]$ESC[37m: $ESC[36m" + ($net2.Status)
-    $Con24 = "$tt" + "Speed  $ESC[31m]$ESC[37m: $ESC[36m" + ($net2.LinkSpeed)
+    if ($net2.Status -eq "Not Present") { $N2tmp = "Disabled"}
+    else { $N2tmp = ($net2.Status) }
+    $Con23 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($N2tmp)
+    $Con24 = "$tt" + "Speed..$ESC[31m]$ESC[37m: $ESC[36m" + ($net2.LinkSpeed)
     $Con31 = "$tt" + "NetName$ESC[31m]$ESC[37m: $ESC[33m" + ($Net3.name)
     $Con32 = "$tt" + "Descrip$ESC[31m]$ESC[37m: $ESC[36m" + ($net3.InterfaceDescription)
-    $Con33 = "$tt" + "Status $ESC[31m]$ESC[37m: $ESC[36m" + ($net3.Status)
-    $Con34 = "$tt" + "Speed  $ESC[31m]$ESC[37m: $ESC[36m" + ($net3.LinkSpeed)
+    if ($net3.Status -eq "Not Present") { $N3tmp = "Disabled"}
+    else { $N3tmp = ($net3.Status) }
+    $Con33 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($N3tmp)
+    $Con34 = "$tt" + "Speed..$ESC[31m]$ESC[37m: $ESC[36m" + ($net3.LinkSpeed)
     $Con41 = "$tt" + "NetName$ESC[31m]$ESC[37m: $ESC[33m" + ($net4.name)
     $Con42 = "$tt" + "Descrip$ESC[31m]$ESC[37m: $ESC[36m" + ($Net4.InterfaceDescription)
-    $Con43 = "$tt" + "Status $ESC[31m]$ESC[37m: $ESC[36m" + ($net4.Status)
-    $Con44 = "$tt" + "Speed  $ESC[31m]$ESC[37m: $ESC[36m" + ($net4.LinkSpeed)
+    if ($net4.Status -eq "Not Present") { $N4tmp = "Disabled"}
+    else { $N4tmp = ($net4.Status) }
+    $Con43 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($N4tmp)
+    $Con44 = "$tt" + "Speed..$ESC[31m]$ESC[37m: $ESC[36m" + ($net4.LinkSpeed)
     $inf0 = "$tt" + "System Information for$ESC[31m]$ESC[37m: $ESC[36m" + $computerSystem.Name
     $inf1 = "$tt" + "Manufacturer$ESC[31m]$ESC[37m: $ESC[36m" + $computerSystem.Manufacturer
     $inf2 = "$tt" + "Model$ESC[31m]$ESC[37m: $ESC[36m" + $computerSystem.Model
@@ -369,18 +370,16 @@ Function MySysInf {
     $inf10 = "$tt" + "CPU$ESC[31m][$ESC[37mStatus$ESC[31m]$ESC[37m: $ESC[36m" + $computerCPU.status
     $inf11 = "$tt" + "CPU$ESC[31m][$ESC[37mManufacturer$ESC[31m]$ESC[37m: $ESC[36m" + $computerCPU.manufacturer
     $inf12 = "$tt" + "HDD$ESC[31m][$ESC[37mC:$ESC[31m][$ESC[37mCapacity$ESC[31m]$ESC[37m: $ESC[36m" + "{0:N2}" -f ($computerHDD1.Size / 1GB) + "$ESC[37mGB"
-    $inf13 = "$tt" + "HDD$ESC[31m][$ESC[37mC:$ESC[31m][$ESC[37mSpace   $ESC[31m]$ESC[37m: $ESC[36m" + "{0:P2}" -f ($computerHDD1.FreeSpace / $computerHDD1.Size) + " $ESC[31m[$ESC[37mFree$ESC[31m]$ESC[37m: $ESC[36m" + "{0:N2}" -f ($computerHDD1.FreeSpace / 1GB) + "$ESC[37mGB"
+    $inf13 = "$tt" + "HDD$ESC[31m][$ESC[37mC:$ESC[31m][$ESC[37mSpace...$ESC[31m]$ESC[37m: $ESC[36m" + "{0:P2}" -f ($computerHDD1.FreeSpace / $computerHDD1.Size) + " $ESC[31m[$ESC[37mFree$ESC[31m]$ESC[37m: $ESC[36m" + "{0:N2}" -f ($computerHDD1.FreeSpace / 1GB) + "$ESC[37mGB"
     $inf14 = "$tt" + "HDD$ESC[31m][$ESC[37mD:$ESC[31m][$ESC[37mCapacity$ESC[31m]$ESC[37m: $ESC[36m" + "{0:N2}" -f ($computerHDD2.Size / 1GB) + "$ESC[37mGB"
-    $inf15 = "$tt" + "HDD$ESC[31m][$ESC[37mD:$ESC[31m][$ESC[37mSpace   $ESC[31m]$ESC[37m: $ESC[36m" + "{0:P2}" -f ($computerHDD2.FreeSpace / $computerHDD2.Size) + " $ESC[31m[$ESC[37mFree$ESC[31m]$ESC[37m: $ESC[36m" + "{0:N2}" -f ($computerHDD2.FreeSpace / 1GB) + "$ESC[37mGB"
-    $inf16 = "$tt" + "System RAM $ESC[31m]$ESC[37m: $ESC[36m" + "{0:N2}" -f ($computerSystem.TotalPhysicalMemory / 1GB) + "$ESC[37mGB"
-    $inf17 = "$tt" + "OS$ESC[31m][$ESC[37mSystem $ESC[31m]$ESC[37m: $ESC[36m" + ($computerOS.caption)
+    $inf15 = "$tt" + "HDD$ESC[31m][$ESC[37mD:$ESC[31m][$ESC[37mSpace...$ESC[31m]$ESC[37m: $ESC[36m" + "{0:P2}" -f ($computerHDD2.FreeSpace / $computerHDD2.Size) + " $ESC[31m[$ESC[37mFree$ESC[31m]$ESC[37m: $ESC[36m" + "{0:N2}" -f ($computerHDD2.FreeSpace / 1GB) + "$ESC[37mGB"
+    $inf16 = "$tt" + "System RAM.$ESC[31m]$ESC[37m: $ESC[36m" + "{0:N2}" -f ($computerSystem.TotalPhysicalMemory / 1GB) + "$ESC[37mGB"
+    $inf17 = "$tt" + "OS$ESC[31m][$ESC[37mSystem.$ESC[31m]$ESC[37m: $ESC[36m" + ($computerOS.caption)
     $inf18 = "$tt" + "OS$ESC[31m][$ESC[37mVersion$ESC[31m]$ESC[37m: $ESC[36m" + ($computerOS.Version)
     $inf19 = "$tt" + "User logged$ESC[31m]$ESC[37m: $ESC[36m" + ($computerSystem.UserName)
     $inf20 = "$tt" + "Last Reboot$ESC[31m]$ESC[37m: $ESC[36m" + ($computerOS.LastBootUpTime)
-    #Clear-Host
     Write-Host $nline
     $tline
-    #$fline
     $dline
     $i = 0
     while ($i -le 36) { Write-Host $sline ; $i++ }
@@ -408,19 +407,22 @@ Function MySysInf {
         $l++
         if ($c -eq 5) { $c = 1; $p++ }
     }
+    [Console]::SetCursorPosition(0, 0)
+    Write-Host -NoNewLine $nline
     [Console]::SetCursorPosition(0, 43)
-    $pop = Read-Host -Prompt  "$ESC[31m[$ESC[37mEnter To Continue$ESC[31m]$ESC[37m"
+    $pop = Read-Host -Prompt "$ESC[31m[$ESC[37mEnter To Continue$ESC[31m]$ESC[37m"
 }
-#Function TheCommand {
-#    [Parameter(Mandatory = $False, ParameterSetName = 'IntCom')]
-#    [ValidateNotNullOrEmpty()]
-#    $IntCom = $_
-#    Write-Host $IntCom
-#    $TC = (Select-String -Pattern $IntCom $Fileini)
-#    $cmd = $TC -split "="
-#    Start-Process $cmd[1] -Verb RunAs
-#}
-#TheCommand
+Function TheCommand {
+    Param([string]$IntCom)
+    if ($IntCom -eq "") {
+        Write-Error "Error In Sent Param " + $IntCom
+        Write-Error $_
+        return
+    }
+    $moo = (Select-String -Pattern $IntCom $Fileini)
+    $cow = $moo -split "="
+    Start-Process $cow[1] -Verb RunAs
+}
 Do {
     #Switch
     Switch (Invoke-Menu -menu $menu -clear) {
@@ -531,6 +533,5 @@ Do {
         }
     } #switch
 } While ($True)
-
 $Filetest = Test-Path -path $Filetmp
 if ($Filetest -eq $true) { Remove-Item –path $Filetmp }
--- Get-SysInfo.ps1
+++ Get-SysInfo.ps1
@@ -1,40 +1,36 @@
-$FileVersion = "0.1.5"
+$FileVersion = "0.1.6"
 $ESC = [char]27
 $nline = "$ESC[31m#=====================================================================#$ESC[37m"
 $dline = "$ESC[31m| $ESC[37m| $ESC[31m#=============================================================$ESC[31m# $ESC[37m| $ESC[31m|"
 $fline = "$ESC[31m| $ESC[37m#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-# $ESC[31m| $ESC[37m"
 $tline = "$ESC[31m| $ESC[37m#=-=-=-=-=-=-=-=-=-=-=-<$ESC[36m[$ESC[37mSystem Information$ESC[36m]$ESC[31m$ESC[37m>=-=-=-=-=-=-=-=-=-=-=#$ESC[31m |$ESC[37m"
 $sline = "$ESC[31m| $ESC[37m| $ESC[31m|                                                             $ESC[31m| $ESC[37m|$ESC[31m |$ESC[37m"
-$host.ui.RawUI.WindowTitle = "System Information Version " +  $FileVersion
-<# #######[Set-ConWin]###[Window Resizer]############################## #>
-$tmpWidth = 72
-$tmpHeight = 48
-if ($tmpWidth -eq "") { $tmpWidth = 107 }
-if ($tmpHeight -eq "") { $tmpHeight = 45 }
-$pshost = get-host
-$pswindow = $pshost.ui.rawui
-$newsize = $pswindow.buffersize
-$newsize.height = 2000
+$host.ui.RawUI.WindowTitle = "System Information Version " + $FileVersion
+<# #[Set-ConWin]#[Window Resizer]# #>
+$tmpHeight = "46"
+$tmpWidth = "72"
+if ($tmpWidth -eq "") { $tmpWidth = "107" }
+if ($tmpHeight -eq "") { $tmpHeight = "45" }
+$pshost = (get-host)
+$pswindow = ($pshost.ui.rawui)
+$newsize = ($pswindow.buffersize)
+$newsize.height = "2000"
 $tmp = ($tmpWidth * 2)
-$newsize.width = $tmp
-$pswindow.buffersize = $newsize
+$newsize.width = "$tmp"
+$pswindow.buffersize = ($newsize)
 $newsize = ($pswindow.windowsize)
-$newsize.height = $tmpHeight
-$newsize.width = $tmpWidth
-$pswindow.windowsize = $newsize
-<# ################################################################ #>
+$newsize.height = ($tmpHeight)
+$newsize.width = ($tmpWidth)
+$pswindow.windowsize = ($newsize)
 $Base = "C:\bin\"
 Get-CimInstance Win32_OperatingSystem | Format-List
 Clear-Host
-$tmpfile = ("$base" + "Get-Sysinfo.tmp")
 $computerSystem = (Get-CimInstance CIM_ComputerSystem)
 $computerBIOS = (Get-CimInstance CIM_BIOSElement)
 $computerOS = (Get-CimInstance CIM_OperatingSystem)
 $computerCPU = (Get-CimInstance CIM_Processor)
 $computerHDD1 = (Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'C:'")
 $computerHDD2 = (Get-CimInstance Win32_LogicalDisk -Filter "DeviceID = 'D:'")
-#$(Get-CimInstance Win32_NetworkAdapter) |  Format-Table -Property name, InterfaceDescription, status, linkspeed
-#$(Get-NetAdapter) | Select-Object -Property name, InterfaceDescription, Status, LinkSpeed |  Export-Csv $tmpfile
 $Net1 = $(Get-NetAdapter)[0] | Select-Object -Property name, InterfaceDescription, Status, LinkSpeed
 $Net2 = $(Get-NetAdapter)[1] | Select-Object -Property name, InterfaceDescription, Status, LinkSpeed
 $Net3 = $(Get-NetAdapter)[2] | Select-Object -Property name, InterfaceDescription, Status, LinkSpeed
@@ -42,19 +38,27 @@ $Net4 = $(Get-NetAdapter)[3] | Select-Object -Property name, InterfaceDescriptio
 $tt = "$ESC[31m[$ESC[37m"
 $Con11 = "$tt" + "NetName$ESC[31m]$ESC[37m: $ESC[33m" + ($Net1.name)
 $Con12 = "$tt" + "Descrip$ESC[31m]$ESC[37m: $ESC[36m" + ($net1.InterfaceDescription)
-$Con13 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($net1.Status)
+if ($net1.Status -eq "Not Present") { $N1tmp = "Disabled"}
+else { $N1tmp = ($net1.Status) }
+$Con13 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($N1tmp)
 $Con14 = "$tt" + "Speed..$ESC[31m]$ESC[37m: $ESC[36m" + ($net1.LinkSpeed)
 $Con21 = "$tt" + "NetName$ESC[31m]$ESC[37m: $ESC[33m" + ($Net2.name)
 $Con22 = "$tt" + "Descrip$ESC[31m]$ESC[37m: $ESC[36m" + ($net2.InterfaceDescription)
-$Con23 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($net2.Status)
+if ($net2.Status -eq "Not Present") { $N2tmp = "Disabled"}
+else { $N2tmp = ($net2.Status) }
+$Con23 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($N2tmp)
 $Con24 = "$tt" + "Speed..$ESC[31m]$ESC[37m: $ESC[36m" + ($net2.LinkSpeed)
 $Con31 = "$tt" + "NetName$ESC[31m]$ESC[37m: $ESC[33m" + ($Net3.name)
 $Con32 = "$tt" + "Descrip$ESC[31m]$ESC[37m: $ESC[36m" + ($net3.InterfaceDescription)
-$Con33 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($net3.Status)
+if ($net3.Status -eq "Not Present") { $N3tmp = "Disabled"}
+else { $N3tmp = ($net3.Status) }
+$Con33 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($N3tmp)
 $Con34 = "$tt" + "Speed..$ESC[31m]$ESC[37m: $ESC[36m" + ($net3.LinkSpeed)
 $Con41 = "$tt" + "NetName$ESC[31m]$ESC[37m: $ESC[33m" + ($net4.name)
 $Con42 = "$tt" + "Descrip$ESC[31m]$ESC[37m: $ESC[36m" + ($Net4.InterfaceDescription)
-$Con43 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($net4.Status)
+if ($net4.Status -eq "Not Present") { $N4tmp = "Disabled"}
+else { $N4tmp = ($net4.Status) }
+$Con43 = "$tt" + "Status.$ESC[31m]$ESC[37m: $ESC[36m" + ($N4tmp)
 $Con44 = "$tt" + "Speed..$ESC[31m]$ESC[37m: $ESC[36m" + ($net4.LinkSpeed)
 $inf0 = "$tt" + "System Information for$ESC[31m]$ESC[37m: $ESC[36m" + $computerSystem.Name
 $inf1 = "$tt" + "Manufacturer$ESC[31m]$ESC[37m: $ESC[36m" + $computerSystem.Manufacturer
@@ -77,10 +81,8 @@ $inf17 = "$tt" + "OS$ESC[31m][$ESC[37mSystem.$ESC[31m]$ESC[37m: $ESC[36m" + ($co
 $inf18 = "$tt" + "OS$ESC[31m][$ESC[37mVersion$ESC[31m]$ESC[37m: $ESC[36m" + ($computerOS.Version)
 $inf19 = "$tt" + "User logged$ESC[31m]$ESC[37m: $ESC[36m" + ($computerSystem.UserName)
 $inf20 = "$tt" + "Last Reboot$ESC[31m]$ESC[37m: $ESC[36m" + ($computerOS.LastBootUpTime)
-#Clear-Host
 Write-Host $nline
 $tline
-#$fline
 $dline
 $i = 0
 while ($i -le 36) { Write-Host $sline ; $i++ }
@@ -108,6 +110,8 @@ while ($l -lt 50) {
     $l++
     if ($c -eq 5) { $c = 1; $p++ }
 }
+[Console]::SetCursorPosition(0, 0)
+Write-Host -NoNewLine $nline
 [Console]::SetCursorPosition(0, 43)
 $pop = Read-Host -Prompt "$ESC[31m[$ESC[37mEnter To Continue$ESC[31m]$ESC[37m"
 if ($pop -eq "") { break }
