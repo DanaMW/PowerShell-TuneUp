7d9576a90362658a9945949e8f2b8feb3deb8542
dana <danamw@gmail.com>
2018-09-08 06:02:54 -0700

    Fixed text placement and percentage

--- ClearLogs.ps1
+++ ClearLogs.ps1
@@ -1,6 +1,6 @@
 Param([bool]$loud)
 #Start-Process -Verb RunAs -FilePath "c:\Windows\System32\wevtutil.exe" -ArgumentList "el | Foreach-Object {wevtutil cl $_}"
-$FileVersion = "Version: 0.1.0"
+$FileVersion = "Version: 0.1.2"
 Write-Host ""
 Write-Host "Running ClearWindows Logs " $FileVersion
 $i = 0
@@ -16,10 +16,16 @@ else {
         wevtutil cl $_
         $i++
         $p = ($i / 11.32)
+        $p = [int][Math]::Ceiling($p)
         Write-Progress -Activity "Clearing Windows Logs" -Status "$p% Complete:" -PercentComplete $p
     }
 }
 
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
+Write-Host ""
 Write-Host ""
 Write-Host "ClearWindows Logs Processed $i log files."
 Read-Host -Prompt "[Slap Enter to Exit]"
