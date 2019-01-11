1cecc5082b0b8d1db78ce2c991de79e6974fc475
dana <danamw@gmail.com>
2019-01-10 23:01:06 -0800

    Count locked files, corrected error handling

--- ClearLogs.ps1
+++ ClearLogs.ps1
@@ -1,6 +1,5 @@
 Param([bool]$loud)
-<# Start-Process -Verb RunAs -FilePath "c:\Windows\System32\wevtutil.exe" -ArgumentList "el | Foreach-Object {wevtutil cl $_}" #>
-$FileVersion = "Version: 0.2.6"
+$FileVersion = "Version: 0.2.8"
 $host.ui.RawUI.WindowTitle = "ClearWindows Logs $FileVersion"
 <# Test and if needed run as admin #>
 Function Test-Administrator {
@@ -17,7 +16,7 @@ Clear-Host
 $PShost = Get-Host
 $PSWin = $PShost.ui.rawui
 $PSWin.CursorSize = 0
-[Console]::SetCursorPosition(0, 3); Say "Running ClearWindows Logs $FileVersion"
+[Console]::SetCursorPosition(0, 3); Say "Running ClearWindows Logs" $FileVersion
 $ClearSet = 0
 & wevtutil el | Foreach-Object { $ClearSet++ }
 [Console]::SetCursorPosition(0, 4); Say "You have $ClearSet logs on this machine. Setting Up the math."
@@ -35,27 +34,38 @@ if ($loud -eq $True) {
     return
 }
 else {
-    wevtutil el | Foreach-Object {
-        [Console]::SetCursorPosition(0, 0)
-        Try { wevtutil cl "$_" -Quiet -ErrorAction SilentlyContinue }
-        catch { Say -NoNewLine ""; Continue }
-        Finally { Say -NoNewLine "" }
-        $i++
-        $p = ($i / $ClearSet); $p = [int][Math]::Ceiling($p)
-        $j = ($p / 2); $j = [int][Math]::Floor($j)
-        [Console]::SetCursorPosition(16, 6); Say -NoNewline "    "
-        [Console]::SetCursorPosition(0, 6); Say -NoNewline "Delete count is: $i"
-        [Console]::SetCursorPosition(16, 7); Say -NoNewline "    "
-        [Console]::SetCursorPosition(0, 7); Say -NoNewline "PercentComplete: $p%"
-        if ($p -eq ($h + 2)) { $h = $p }; $it = "#" * $j
-        IF (($p % 2) -eq 1 -and $h -le $p) { $tip = "="; $it = ($it + $tip) }
-        IF (($p % 2) -eq 0 -and $h -gt $p) { $tip = "#"; $it = ($it + $tip) }
-        [Console]::SetCursorPosition(0, 8); Say -NoNewline "[$it"
-        [Console]::SetCursorPosition(51, 8); Say -NoNewline "]"
+    $ec = 0
+    $OrgError = $ErrorActionPreference
+    $ErrorActionPreference = "Ignore"
+    Try {
+        & wevtutil el | ForEach-Object {
+            Try { & wevtutil cl "$_" }
+            catch { Continue }
+            if ($LastExitCode -ne 0) { $ec++ }
+            $i++
+            $p = ($i / $ClearSet); $p = [int][Math]::Ceiling($p)
+            $j = ($p / 2); $j = [int][Math]::Floor($j)
+            [Console]::SetCursorPosition(16, 6); Say -NoNewline "    "
+            [Console]::SetCursorPosition(0, 6); Say -NoNewline "Delete count is: $i"
+            [Console]::SetCursorPosition(16, 7); Say -NoNewline "    "
+            [Console]::SetCursorPosition(0, 7); Say -NoNewline "PercentComplete: $p%"
+            #[Console]::SetCursorPosition(16, 8); Say -NoNewline "                                                                                              "
+            #[Console]::SetCursorPosition(0, 8); Say -NoNewline $error.count $error[0]
+            if ($p -eq ($h + 2)) { $h = $p }; $it = "#" * $j
+            if (($p % 2) -eq 1 -and $h -le $p) { $tip = "="; $it = ($it + $tip) }
+            if (($p % 2) -eq 0 -and $h -gt $p) { $tip = "#"; $it = ($it + $tip) }
+            [Console]::SetCursorPosition(0, 9); Say -NoNewline "[$it"
+            [Console]::SetCursorPosition(51, 9); Say -NoNewline "]"
+            [Console]::SetCursorPosition(0, 8); Say -NoNewline "                                                                                              "
+            [Console]::SetCursorPosition(0, 8); Say -NoNewLine ""
+        }
     }
+    Catch { Continue }
+    $ErrorActionPreference = $OrgError
     asay ClearWindows Logs Processed $i log files.
-    [Console]::SetCursorPosition(0, 9); Say -NoNewline "ClearWindows Logs Processed $i log files."
-    [Console]::SetCursorPosition(0, 10); Read-Host -Prompt "[Slap Enter to Exit]"
+    [Console]::SetCursorPosition(0, 11); Say -NoNewline "ClearWindows Logs Processed $i log files."
+    [Console]::SetCursorPosition(0, 12); Say -NoNewline $ec "Files were in use and not cleared."
+    [Console]::SetCursorPosition(0, 13); Read-Host -Prompt "[Slap Enter to Exit]"
     $PShost = Get-Host
     $PSWin = $PShost.ui.rawui
     $PSWin.CursorSize = 25
