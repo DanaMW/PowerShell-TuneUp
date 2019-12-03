e10cb2fe6740ff5365053d5394aa271167f13ce6
DanaMW <danamw@gmail.com>
2019-12-03 08:00:52 -0800

    Update Check-Prof.ps1

--- Check-Prof.ps1
+++ Check-Prof.ps1
@@ -1,15 +1,13 @@
-﻿$FileVersion = "Version: 0.0.8"
+﻿$FileVersion = "Version: 0.0.9"
 $txtfile = ($env:BASE + "\check-prof.ini")
 $Filetest = Test-Path -path $txtfile
-if ($Filetest -eq $true) {
-    Say 'The File $txtfile exists, clearing file.'
-    Clear-Content -Path $txtfile
-}
+if ($Filetest -eq $true) { Clear-Content -Path $txtfile }
 #Save below 2 line very useful amd both working
 #$Profile | Format-List -Force | Out-String -Stream| % {New-Variable "prof$i" $_ ; $i++}
 $Profile | Format-List -Force | Out-String | ForEach-Object { $carrydata = $_ }
 $carrydata | Format-List | Out-file $txtfile
 $i = 3
+Remove-Empty $txtfile
 $reader = [System.IO.File]::OpenText($txtfile)
 try {
     $line1 = $reader.ReadLine()
@@ -20,62 +18,57 @@ try {
     if ($null -eq $line3) { break }
     $line4 = $reader.ReadLine()
     if ($null -eq $line4) { break }
-    $line5 = $reader.ReadLine()
-    if ($null -eq $line5) { break }
-    $line6 = $reader.ReadLine()
-    if ($null -eq $line6) { break }
     $reader.Close()
 }
 finally { $reader.Close() }
 $writer = [System.IO.file]::CreateText($txtfile)
 try {
+    $moo = $line1 -replace "\s", ""
+    $cow = $moo -split ":", 2
+    $writer.Writeline($cow[0])
+    $writer.Writeline($cow[1])
+    if ($null -eq $cow) { break }
+    $moo = $line2 -replace "\s", ""
+    $cow = $moo -split ":", 2
+    $writer.Writeline($cow[0])
+    $writer.Writeline($cow[1])
+    if ($null -eq $cow) { break }
     $moo = $line3 -replace "\s", ""
     $cow = $moo -split ":", 2
-    $writer.Writeline("[1A]=" + $cow[0])
-    $writer.Writeline("[1B]=" + $cow[1])
+    $writer.Writeline($cow[0])
+    $writer.Writeline($cow[1])
     if ($null -eq $cow) { break }
     $moo = $line4 -replace "\s", ""
     $cow = $moo -split ":", 2
-    $writer.Writeline("[2A]=" + $cow[0])
-    $writer.Writeline("[2B]=" + $cow[1])
-    if ($null -eq $cow) { break }
-    $moo = $line5 -replace "\s", ""
-    $cow = $moo -split ":", 2
-    $writer.Writeline("[3A]=" + $cow[0])
-    $writer.Writeline("[3B]=" + $cow[1])
-    if ($null -eq $cow) { break }
-    $moo = $line6 -replace "\s", ""
-    $cow = $moo -split ":", 2
-    $writer.Writeline("[4A]=" + $cow[0])
-    $writer.Writeline("[4B]=" + $cow[1])
+    $writer.Writeline($cow[0])
+    $writer.Writeline($cow[1])
     if ($null -eq $cow) { break }
     $writer.Close()
 }
 finally { $writer.Close() }
 Clear-Host
-Say "System Profiles information $FileVersion"
+WC "~white~System Profiles information $FileVersion~"
 Say ""
-Say "The Main Profile For Your Account is:"
-Say "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
-Say $Profile
+WC "~darkyellow~The Main Profile For Your Account is~~white~:~"
+WC "~white~=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=~"
+WC "~yellow~$Profile~"
 Say ""
-Say "The other possible profile files are:"
-Say "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
-Say ""
+WC "~darkyellow~The other possible profile files are~~white~:~"
+WC "~white~=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=~"
 $l = 11
 $i = 0
 while ($i -le 7) {
     $Line = (Get-Content $txtfile)[$i]
-    $Moo = $line -split "="
+    $Moo = $line
     $i++; $l++
     $Line = (Get-Content $txtfile)[$i]
-    $Cow = $line -split "="
-    Say "["$Moo[1]"]: "$Cow[1]
-    $i++; $l++; Say ""
+    $Cow = $line
+    WC "~darkcyan~[~~white~$Moo~~darkcyan~]~~white~:~ ~yellow~$Cow~"
+    $i++; $l++;
 }
 $l++; $l++
-Say ""
 $Filetest = Test-Path -path $txtfile
 if ($Filetest -eq $true) { Remove-Item -Path $txtfile }
-$tmp = WCP "~DARKCYAN~[~~WHITE~Tap Enter to Exit~~DARKCYAN~]~~WHITE~:~ "
+Say ""
+$tmp = WCP "~DARKCYAN~[~~darkyellow~Tap Enter to Exit~~DARKCYAN~]~~WHITE~:~ "
 Read-Host -Prompt $tmP
