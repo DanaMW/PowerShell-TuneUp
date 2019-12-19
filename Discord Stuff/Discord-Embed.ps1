b735a159855360e09b7953f15db7ec6c4a336cc8
DanaMW <danamw@gmail.com>
2019-12-19 02:04:35 -0800

    discord stuff update

--- Discord Stuff/Discord-Embed.ps1
+++ Discord Stuff/Discord-Embed.ps1
@@ -1,14 +1,15 @@
-param([string]$MyArgs)
-$FileVersion = "Version: 0.0.6"
+﻿param([string]$MyArgs)
+$FileVersion = "Version: 0.0.8"
+$Space = "       "
 $TheArgs = "$myargs $args"
-$hookUrl = 'https://discordapp.com/api/webhooks/YourWebHookURLHere'
+$hookUrl = 'WebHook URL'
 $title0 = $Env:USERDOMAIN
-$title = $title0.substring(0, 1).toupper() + $title0.substring(1).tolower()
+$title = $title0.substring(0, 1).ToUpper() + $title0.substring(1).ToLower()
 if ($MyArgs -eq "") {
     $content = @"
 {
  "embeds": [{
-   "title": "System Information $FileVersion From: $title",
+  "title": "Server Message From: $title",
    "description": "Error: No value was included as a parameter",
    "color": 16711680
  }]
@@ -21,7 +22,7 @@ else {
     $content = @"
 {
  "embeds": [{
-   "title": "System Information $FileVersion From: $title",
+   "title": "Server Message From: $title",
    "description": "$TheArgs",
    "color": 16711680
  }]
@@ -30,3 +31,7 @@ else {
     Invoke-RestMethod -Uri $hookUrl -Method Post -Body $content -ContentType 'application/json'
     return
 }
+#$thumbnailObject = [PSCustomObject]@{
+#    url = "https://static1.squarespace.com/static/5644323de4b07810c0b6db7b/t/5aa44874e4966bde3633b69c/1520715914043/webhook_resized.png"
+#}
+#"thumbnail": "$thumbnailObject"
--- Discord Stuff/Discord-Whook.ps1
+++ Discord Stuff/Discord-Whook.ps1
@@ -1,13 +1,14 @@
-param([string]$MyArgs)
-$FileVersion = "Version: 0.0.4"
+﻿param([string]$MyArgs)
+$FileVersion = "Version: 0.0.8"
+$Space = "       "
 if ($myargs -eq "") {
-    $hookUrl = 'https://discordapp.com/api/webhooks/YourWebhookUrl'
+    $hookUrl = 'WebHook URL'
     $title0 = $Env:USERDOMAIN
     $title = $title0.substring(0, 1).toupper() + $title0.substring(1).tolower()
     $content = @"
-System Information From: $title
+Server Message $title
 No value was included as a parameter.
-Webhook: WzErO
+$Space
 "@
     $payload = [PSCustomObject]@{
         content = $content
@@ -16,17 +17,23 @@ Webhook: WzErO
     return
 }
 $TheArgs = "$myargs $args"
-$hookUrl = 'https://discordapp.com/api/webhooks/471880135226949633/YourWebhookUrl'
+$hookUrl = 'WebHook URL'
 $title0 = $Env:USERDOMAIN
 $title = $title0.substring(0, 1).toupper() + $title0.substring(1).tolower()
 
 $content = @"
-System Information From: $title
+Server Message From: $title
 $theArgs
+$Space
 "@
 
 $payload = [PSCustomObject]@{
     content = $content
 }
-Write-Host $payload
+#Say $payload
 Invoke-RestMethod -Uri $hookUrl -Method Post -Body ($payload | ConvertTo-Json) -ContentType 'application/json'
+
+#$thumbnailObject = [PSCustomObject]@{
+#    url = "https://static1.squarespace.com/static/5644323de4b07810c0b6db7b/t/5aa44874e4966bde3633b69c/1520715914043/webhook_resized.png"
+#}
+#"thumbnail": "$thumbnailObject"
