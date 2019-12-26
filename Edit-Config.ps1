2206df597a73ed247ed633a9ebcbe00484cdda49
DanaMW <danamw@gmail.com>
2019-12-25 23:38:34 -0800

    Update Edit-Config.ps1

--- Edit-Config.ps1
+++ Edit-Config.ps1
@@ -3,25 +3,52 @@
         Edit-Config
         Created By: Dana Meli
         Created Date: December, 2019
-        Last Modified Date: December 25, 2019
+        Last Modified Date: December 26, 2019
 
 .DESCRIPTION
         This script is designed to read and edit json configuration files for all PowerShell scripts.
         Also used as the settings manager for the json configuration files.
 
 .EXAMPLE
-        Edit-Config -Confile [string] -Read [string] -Write [string] -Section [string] -SValue [string] -BValue [bool]
+        Edit-Config -Confile [string] -Count [string] -Read [string] -Write [string] -Section [string] -SValue [string] -BValue [bool]
 
 .NOTES
         Still under development.
 
 #>
-param([string]$Confile, [string]$Read, [string]$Write, [string]$Section, [string]$SValue, [bool]$BValue)
-$FileVersion = "Version: 0.0.2"
+[CmdLetBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$Confile,
+    [Parameter(Mandatory = $false)]
+    [AllowEmptyString()]
+    [string]$Count,
+    [Parameter(Mandatory = $false)]
+    [string]$Read,
+    [Parameter(Mandatory = $false)]
+    [string]$Write,
+    [Parameter(Mandatory = $false)]
+    [string]$Section,
+    [Parameter(Mandatory = $false)]
+    [string]$SValue,
+    [Parameter(Mandatory = $false)]
+    [bool]$BValue,
+    [Parameter(Mandatory = $false)]
+    [string]$Sub
+)
+$FileVersion = "Version: 0.0.3"
 $Script:ConfigFile = $Confile
 try { $Script:Config = Get-Content $Configfile -Raw | ConvertFrom-Json }
 catch { Say -ForeGroundColor RED "Edit-Config $FileVersion - The file $Confile is missing!"; break }
-if (($Read)) { $Feedback = ($Config.$Read.$Section) }
+if (($Count)) { $Feedback = ($Config.$Count).count }
+if (($Read)) {
+    if (($Sub)) { $Feedback = ($Config.$Read)[$Section].$Sub }
+    else {
+        #use [double] for dec nums (1.5)
+        if ($section -is [int]) { $Feedback = ($Config.$Read)[$Section] }
+        else { $Feedback = ($Config.$Read.$Section) }
+    }
+}
 if (($Write)) {
     if (($SValue)) {
         $Config.$Write.$Section = [string]$SValue
