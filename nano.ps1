e957baf7238ede0c4603201a9f9ce919893a1518
DanaMW <danamw@gmail.com>
2020-01-22 10:40:20 -0800

    Update nano.ps1

--- nano.ps1
+++ nano.ps1
@@ -1,11 +1,11 @@
 $MyArgs = ($args)
-$FileVersion = "Version: 0.0.3"
+$FileVersion = "Version: 0.0.4"
 $drive = (Split-Path -parent $MyArgs)
 $file = (Split-Path -leaf $MyArgs)
 if (!($drive)) { bash -c "nano $File" }
 else {
     $drive = $drive.tolower()
-    $drive = $drive.replace("\:", "/")
+    $drive = $drive.replace(":\", "/")
     $drive = ("/mnt/" + $drive + "/" + $file)
     $file = $drive
     bash -c "nano $File"
