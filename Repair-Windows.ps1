c679c3c707edb29e31fb717df9d3d1368ddb0ef8
Dana Meli <DanaMW@gmail.com>
2021-01-30 13:24:29 -0800

    Update Repair-Windows.ps1

--- Repair-Windows.ps1
+++ Repair-Windows.ps1
@@ -1,5 +1,5 @@
 Param([string]$RunFix)
-$FileVersion = "Version: 0.1.9"
+$FileVersion = "Version: 0.2.0"
 $host.ui.RawUI.WindowTitle = "Fix Windows Version " + $FileVersion
 function Test-Administrator {
     $user = [Security.Principal.WindowsIdentity]::GetCurrent();
@@ -14,6 +14,7 @@ if (!($principal.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator
 }
 $Scan = "SFC.EXE /SCANNOW"
 $Check = "DISM.EXE /Online /Cleanup-Image /ScanHealth"
+$Image = "DISM /Online /Cleanup-Image /CheckHealth" # That works by repairing the Windows system image files.
 $Repair = "DISM.EXE /Online /Cleanup-Image /RestoreHealth /Source:WIM:E:\sources\install.wim:1"
 $Reset = "DISM.EXE /Online /Cleanup-Image /StartComponentCleanup /Source:WIM:E:\sources\install.wim:1"
 Set-Location "C:"; Set-Location "C:\Windows"
@@ -40,6 +41,7 @@ $newsize.width = ($tmpWidth)
 $pswindow.windowsize = ($newsize)
 if ($RunFix -eq "SCAN") { $RunFix = "Scan" }
 if ($RunFix -eq "CHECK") { $RunFix = "Check" }
+if ($RunFix -eq "IMAGE") { $RunFix = "IMAGE" }
 if ($RunFix -eq "REPAIR") { $RunFix = "Repair" }
 if ($RunFix -eq "RESET") { $RunFix = "Reset" }
 if ($RunFix -eq "") { $RunFix = "HelpOnly" }
@@ -61,7 +63,7 @@ if ($RunFix -eq "HelpOnly") {
     [Console]::SetCursorPosition(7, 4); WC "~WHITE~Scan, Check and/or Repair your Windows 10 PC.~"
     [Console]::SetCursorPosition(7, 5); WC "~WHITE~If this is your first time running it you~"
     [Console]::SetCursorPosition(7, 6); WC "~WHITE~need to run it in a certain order. The order is:~"
-    [Console]::SetCursorPosition(7, 7); WC "~WHITE~(1)Scan, (2)Check, (3)Repair (4)Reset~"
+    [Console]::SetCursorPosition(7, 7); WC "~WHITE~(1)Scan, (2)Check, (3)Image, (4)Repair, (5)Reset~"
     [Console]::SetCursorPosition(7, 8); WC "~WHITE~After each run you need to reboot. Normally you need~"
     [Console]::SetCursorPosition(7, 9); WC "~WHITE~only run ~~CYAN~[~~YELLOW~Repair-Windows SCAN~~CYAN~] ~~WHITE~to check it.~"
     [Console]::SetCursorPosition(7, 10); WC "~WHITE~For a good check run that, reboot, then run~"
@@ -82,7 +84,7 @@ If ($RunFix -eq "") {
     WC $tline
     WC $dline
     $i = 1
-    while ($i -lt "7") {
+    while ($i -lt "8") {
         WC $sline
         $i++
     }
@@ -92,16 +94,18 @@ If ($RunFix -eq "") {
     [Console]::SetCursorPosition(7, 3); WC "You Must Run this in a certain order:"
     [Console]::SetCursorPosition(7, 4); WC "(1) Repair-Windows SCAN   [OK to run anytime]"
     [Console]::SetCursorPosition(7, 5); WC "(2) Repair-Windows CHECK  [OK to run anytime]"
-    [Console]::SetCursorPosition(7, 6); WC "(3) Repair-Windows REPAIR [Only if the last two don't do it]"
-    [Console]::SetCursorPosition(7, 7); WC "(4) Repair-Windows RESET  [Redoes your machine, last resort]"
-    [Console]::SetCursorPosition(7, 8); WC "(H)elp or (Q)uit"
-    [Console]::SetCursorPosition(0, 12)
-    $menuPrompt = WCP "~CYAN~[~~darkyellow~Select 1, 2, 3, 4, H or Q~~CYAN~]~~WHITE~:~ "
+    [Console]::SetCursorPosition(7, 6); WC "(3) Repair-Windows IMAGE  [Run to repair image]"
+    [Console]::SetCursorPosition(7, 7); WC "(4) Repair-Windows REPAIR [Only if the last two don't do it]"
+    [Console]::SetCursorPosition(7, 8); WC "(5) Repair-Windows RESET  [Redoes your machine, last resort]"
+    [Console]::SetCursorPosition(7, 9); WC "(H)elp or (Q)uit"
+    [Console]::SetCursorPosition(0, 13)
+    $menuPrompt = WCP "~CYAN~[~~darkyellow~Select 1, 2, 3, 4, 5, H or Q~~CYAN~]~~WHITE~:~ "
     $cmd = Read-Host -Prompt $menuPrompt
     if ($cmd -eq "1") { $RunFix = "SCAN" }
     if ($cmd -eq "2") { $RunFix = "CHECK" }
-    if ($cmd -eq "3") { $RunFix = "REPAIR" }
-    if ($cmd -eq "4") { $RunFix = "RESET" }
+    if ($cmd -eq "3") { $RunFix = "IMAGE" }
+    if ($cmd -eq "4") { $RunFix = "REPAIR" }
+    if ($cmd -eq "5") { $RunFix = "RESET" }
     if ($cmd -eq "H") { Start-Process "pwsh.exe" -ArgumentList "$PSScriptRoot\Repair-Windows.ps1" -Verb RunAs; break }
     if ($cmd -eq "Q") { Clear-Host; break }
 }
@@ -119,12 +123,13 @@ while ($i -lt "5") {
 WC $dline
 WC $fline
 WC $nline
-[Console]::SetCursorPosition(11, 3); WC "~cyan~[~~white~1~~cyan~] ~~green~Scan ~-- ~cyan~[~~white~2~~cyan~] ~~green~Check ~-- ~cyan~[~~white~3~~cyan~] ~~green~Repair ~-- ~cyan~[~~white~4~~cyan~] ~~green~Reset~"
+[Console]::SetCursorPosition(11, 3); WC "~cyan~[~~white~1~~cyan~] ~~green~Scan ~-- ~cyan~[~~white~2~~cyan~] ~~green~Check ~-- ~cyan~[~~white~3~~cyan~] ~~green~Image ~-- ~cyan~[~~white~4~~cyan~] ~~green~Repair ~-- ~cyan~[~~white~5~~cyan~] ~~green~Reset~"
 [Console]::SetCursorPosition(16, 5); WC "   ~cyan~Currently Running the Process~~white~: ~~white~$RunFix~"
 $pa = 10
 [Console]::SetCursorPosition(0, $pa)
 if ($RunFix -eq "Scan") { Invoke-Expression $Scan }
 if ($RunFix -eq "Check") { Invoke-Expression $Check }
+if ($RunFix -eq "IMAGE") { Invoke-Expression $Image }
 if ($RunFix -eq "Repair") { Invoke-Expression $Repair }
 if ($RunFix -eq "Reset") { Invoke-Expression $Reset }
 $menuPrompt = WCP "~CYAN~[~~darkyellow~Enter To Continue~~CYAN~]~~WHITE~:~ "
