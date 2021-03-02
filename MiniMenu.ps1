99c2a671d286e6e11534a1aa25c0a17e98ea93e7
Dana Meli <DanaMW@gmail.com>
2021-03-02 03:14:01 -0800

    Update MiniMenu.ps1

--- MiniMenu.ps1
+++ MiniMenu.ps1
@@ -1,4 +1,4 @@
-$FileVersion = "0.0.2"
+$FileVersion = "0.0.3"
 $MenuOptions = @("PowerShell Core", "Total Commander", "VS Code", "MusicBee");
 $MenuRuns = @("$env:ShellSpec", "tc.ps1", "Code.exe", "D:\bin\Musicbee\MusicBee.exe");
 [int]$Count = $MenuOptions.count
@@ -8,10 +8,14 @@ while ($i -le $Count) {
     Say [$i] $MenuOptions[$i]
     $i++
 }
-$Ans = read-Host -Prompt "Menu Option, (Q)uit"
+$Ans = read-Host -Prompt "Select a Menu Option or (Q)uit"
 if ($Ans -eq "Q" -or $Ans -eq "") { break }
 Say $MenuRuns[$Ans]
 # Use
 # $MenuRuns[$Ans]
 # Or use
-& $MenuRuns[$Ans]
+try {
+    & $MenuRuns[$Ans]
+}
+catch { break }
+break
