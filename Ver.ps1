fd3d453a8a85f115af7a772af13ecb50825bf655
DanaMW <danamw@gmail.com>
2021-03-18 01:06:21 -0700

    Update Ver.ps1

--- Ver.ps1
+++ Ver.ps1
@@ -1,23 +1,42 @@
-﻿$FileVersion = "0.1.7"
+﻿<#
+.SYNOPSIS
+        Ver
+        Created By: Dana Meli
+        Created Date: December, 2019
+        Last Modified Date: March 18, 2021
+
+.DESCRIPTION
+        My idea of a Windows version script.
+
+.EXAMPLE
+        VER
+
+.NOTES
+        Still under development.
+
+#>
+$FileVersion = "0.2.0"
 Clear-Host
 if ($env:HOME -match "C:\\Users\\") {
     Say "Detected Windows Processing"
     Start-Sleep -S 1
     Clear-Host
     $VarFile = ($env:BASE + "\ver.tmp")
-    $Filetest = Test-Path -path $VarFile
-    if ($Filetest -eq $True) { Remove-Item –path $VarFile }
+    $Filetest = Test-Path -Path $VarFile
+    if ($Filetest -eq $True) { Remove-Item –Path $VarFile }
     [Console]::SetCursorPosition(0, 0)
     Say ""
     Say -ForegroundColor white "My Version Information $FileVersion"
     Say -ForegroundColor red "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-";
     Get-SmallVer;
     $computerOS = (Get-CimInstance CIM_OperatingSystem)
-    Say ($computerOS.caption) ($computerOS.version)
+    $TC1 = ($computerOS.caption)
+    $TC2 = ($computerOS.version)
+    WC "~darkcyan~[~~darkyellow~$TC1 $TC2~~darkcyan~]~"
     [Console]::SetCursorPosition(0, 5)
     systeminfo /fo csv | ConvertFrom-Csv | Select-Object OS*, System*, Hotfix* | Format-List | Out-File $VarFile
-    (Get-Content $VarFile) | Where-Object { $_.trim() -ne "" } | set-content $VarFile
-    $lines = (Get-content $VarFile).count
+    (Get-Content $VarFile) | Where-Object { $_.trim() -ne "" } | Set-Content $VarFile
+    $lines = (Get-Content $VarFile).count
     $i = 0
     $j = 1
     $p = 5
@@ -28,11 +47,11 @@ if ($env:HOME -match "C:\\Users\\") {
         $i++; $j++; $p++
     }
     # [Console]::SetCursorPosition(0, 4); Say -ForegroundColor red "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"; [Console]::SetCursorPosition(0, 19);
-    $Filetest = Test-Path -path $VarFile
-    if ($Filetest -eq $True) { Remove-Item –path $VarFile }
+    $Filetest = Test-Path -Path $VarFile
+    if ($Filetest -eq $True) { Remove-Item –Path $VarFile }
     return
 }
 else {
-    Say "Detected NOT windows, not going to run here yet sorry, Exiting"
+    Say "Did not detect Windows. Working on the linux version right this second. Exiting"
     return
 }
